8a5fe41286c52d65dbf9f0f807574f920a00f578
Dream0627 <2551487450@qq.com>
2026-09-10 20:31:00 +0800

    start

---
+++ config/config.go
@@ -0,0 +1,58 @@
+package config
+
+import (
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+type ServerConfig struct {
+	Port int `mapstructure:"port"`
+}
+
+type DatabaseConfig struct {
+	Enabled  bool   `mapstructure:"enabled"`
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
+	Name     string `mapstructure:"name"`
+}
+
+type Config struct {
+	Server   ServerConfig `mapstructure:"server"`
+	Database DatabaseConfig `mapstructure:"database"`
+	JWTConfig JWTConfig `mapstructure:"jwt"`
+}
+
+type JWTConfig struct {
+    Secret string `mapstructure:"secret"`
+	ExpireSeconds int64 `mapstructure:"expires"`
+	Issuer string `mapstructure:"issuer"`
+}
+
+func Load() (*Config, error) {
+	viper.SetConfigFile("config/config.yaml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("读取配置失败", err)
+		return nil, err
+	}
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		fmt.Println("解析配置失败", err)
+		return nil, err
+	}
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("配置已改变", e.Name)
+		if err := viper.Unmarshal(&cfg); err != nil {
+			fmt.Println("重新加载配置失败", err)
+			return 
+		}
+		fmt.Println("重新加载配置成功",e.Name)
+	})
+
+	return &cfg, nil
+}
